dadea260359e6877a28fa34a9e2a852db3210678
Hollote <ukrainetsvik@gmail.com>
2019-03-04 14:33:05 +0200

    add styled-component package, basic styles

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
+
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
 
 class App extends Component {
   render() {
     return (
-      <div> Hello World</div>
+      <div>
+        <WelcomeMessage/>
+      </div>
     );
   }
 }
