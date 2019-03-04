96e9c7f6a34758888ec9f23df74bfd91f2726683
Hollote <ukrainetsvik@gmail.com>
2019-03-04 14:39:51 +0200

    add appLayout

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import styled, { css } from 'styled-components';
 
+import AppLayout from './AppLayout';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
         <WelcomeMessage/>
-      </div>
+      </AppLayout>
     );
   }
 }
