a04f402af559c86c9b4fa52153d9575f2614ca42
Hollote <ukrainetsvik@gmail.com>
2019-03-05 15:43:25 +0200

    add Search

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -6,7 +6,7 @@ import CoinTile from '../Settings/CoinTile';
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(13  0px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
   grid-gap: 15px;
   margin-top: 40px;
 `;
