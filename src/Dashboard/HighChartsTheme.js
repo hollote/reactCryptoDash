09ed1266cb13e684b4285862d35dfb0c8afdf2d1
Hollote <ukrainetsvik@gmail.com>
2019-03-06 16:47:20 +0200

    change to lightTheme

--- src/Dashboard/HighChartsTheme.js
+++ src/Dashboard/HighChartsTheme.js
@@ -1,3 +1,5 @@
+import {lightTheme} from "../Shared/Styles";
+
 export default {
   colors: [
     '#61d936',
@@ -5,11 +7,11 @@ export default {
     '#1163c9'
   ],
   chart: {
-    backgroundColor: '#061A44',
+    backgroundColor: lightTheme ? 'white' : '#061A44',
     borderColor: '#000000',
     borderWidth: 0,
     className: 'dark-container',
-    plotBackgroundColor: '#061A44',
+    plotBackgroundColor: lightTheme ? 'white' : '#061A44',
     plotBorderWidth: 0
   },
   title: {
